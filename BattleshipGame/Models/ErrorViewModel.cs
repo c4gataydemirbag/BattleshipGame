d0f730398cfbccef5f675d12da8bac03fc374d42
cagat <cagat@DESKTOP-LQUOQG2>
2025-12-11 14:36:46 +0300

    Proje dosyası ekle.

---
+++ BattleshipGame/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BattleshipGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
